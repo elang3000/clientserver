4ffb5e4397002da8450b931a9722478a80a30464
elang3000 <40287742+elang3000@users.noreply.github.com>
2018-06-25 22:54:22 +0800

    new project
    new project

---
+++ src/main/java/com/imooc/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+	//aaaas
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
